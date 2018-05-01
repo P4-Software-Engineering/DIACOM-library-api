6557eed2f39464018823b212fa26cbbf49302613
GabriellBP <gbp@ic.ufal.br>
2018-05-01 16:51:08 -0300

    Configuração das permissões e associação de permissões em cada view

--- library/serializers.py
+++ library/serializers.py
@@ -3,16 +3,10 @@ from rest_auth.registration.serializers import RegisterSerializer
 
 try:
     from allauth.account import app_settings as allauth_settings
-    from allauth.utils import (email_address_exists,
-                               get_username_max_length)
     from allauth.account.adapter import get_adapter
     from allauth.account.utils import setup_user_email
-    from allauth.socialaccount.helpers import complete_social_login
-    from allauth.socialaccount.models import SocialAccount
-    from allauth.socialaccount.providers.base import AuthProcess
 except ImportError:
     raise ImportError("allauth needs to be added to INSTALLED_APPS.")
-
 
 from rest_framework import serializers
 from .models import Donation, Location, NominalBook, Book, MyUser as User
--- library/views.py
+++ library/views.py
@@ -1,33 +1,57 @@
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, mixins
 from rest_framework.authentication import SessionAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from library.permissions import IsDIACOM, IsDIACOMOrReadOnly, IsOwnerOrDIACOM
 from .models import NominalBook as NominalBookModel, Book as BookModel, Location as LocationModel, Donation as DonationModel, MyUser as MyUserModel
 from .serializers import BookSerializer, UserSerializer, NominalBookSerializer, LocationSerializer, DonationSerializer
 
 
 class NominalBook(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly, IsDIACOMOrReadOnly]
+
     queryset = NominalBookModel.objects.all()
     serializer_class = NominalBookSerializer
 
 
 class Book(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated, IsDIACOM]
+
     queryset = BookModel.objects.all()
     serializer_class = BookSerializer
 
 
-class Location(viewsets.ModelViewSet):
+class Location(viewsets.GenericViewSet,
+               mixins.CreateModelMixin,
+               mixins.ListModelMixin,
+               mixins.UpdateModelMixin,
+               mixins.RetrieveModelMixin):
+    def get_permissions(self):
+        if self.action in ('list', 'create', 'update'):
+            permission_classes = [IsAuthenticated, IsDIACOM]
+        else:
+            permission_classes = [IsAuthenticated, IsOwnerOrDIACOM]
+
+        return [permission() for permission in permission_classes]
+
     queryset = LocationModel.objects.all()
     serializer_class = LocationSerializer
 
 
 class Donation(viewsets.ModelViewSet):
+    def get_permissions(self):
+        if self.action == 'create':
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [IsAuthenticated, IsDIACOM]
+
+        return [permission() for permission in permission_classes]
+
     queryset = DonationModel.objects.all()
     serializer_class = DonationSerializer
-
 
 
 # class UserList(generics.ListCreateAPIView):
